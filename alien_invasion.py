102f8fd0861926c759c4efbce2388dad694d9d97
mRcfps <1402491442@qq.com>
2016-12-27 21:16:55 +0800

    alien_invasion.py # the basic structure of the game

---
+++ alien_invasion.py
@@ -0,0 +1,22 @@
+import sys
+
+import pygame
+
+
+def run_game():
+    # initialize the game and create a screen object
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    # start the main game loop
+    while True:
+        # listen to the events of the keyboard and mouse
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # make the latest display visible
+        pygame.display.flip()
+
+run_game()
