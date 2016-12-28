310dcca4729918acbe6b17030dceb4efbf24e87d
mRcfps <1402491442@qq.com>
2016-12-28 10:58:26 +0800

    Refactor the moving logic.

--- ship.py
+++ ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship(object):
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """initialize the ship and set its inital position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # load the ship image and get its bounding rect
         self.image = pygame.image.load('images/ship.png')
@@ -15,6 +16,23 @@ class Ship(object):
         # move each new ship to the bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
+
+        # moving flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """adjust the position of ship according to the moving flag"""
+        # update the value of the ship's center instead of rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # update rect according to self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """draw a ship at the assigned position"""
